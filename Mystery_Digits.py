e69b265f78f9fc65e8443d3a73834bc78bbbd3fb
Sami Bilgehan Yörük <122204072+Codellsby@users.noreply.github.com>
2023-05-30 16:08:42 +0300

    Update Mystery_Digits.py
    The Mystery Digits game is a number guessing game where the player tries to guess a 5-digit number. The objective is to determine the secret number by making guesses and receiving feedback on the correctness of the digits.
    
    When the game starts, the player is presented with a menu. The menu provides the following options:
    
    Play Game: This option allows the player to start a new game. A secret number is generated randomly, consisting of five digits. The player enters their guess, a 5-digit number, and the game provides feedback based on the digits' positions in the guess. A '+' sign indicates a correct digit in the correct position, a '?' sign indicates a correct digit in the wrong position, and a '-' sign indicates an incorrect digit. The player continues making guesses until they guess the correct number, and the game displays the number of attempts made.
    
    About: This option provides information about the Mystery Digits game. It explains the rules and objective of the game, guiding the player on how to play and what to expect during the guessing process. It encourages the player to have fun while attempting to guess the secret number.
    
    Quit: Selecting this option allows the player to exit the game. The game displays a message confirming that the game is quitting, and the program terminates.
    
    The game menu is displayed in a user-friendly format, allowing the player to easily navigate and select their desired option. Invalid choices are handled, and the player is prompted to enter a valid option.
    
    Enjoy playing the Mystery Digits game and challenge yourself to guess the secret number in the fewest attempts possible!

--- Mystery_Digits.py
+++ Mystery_Digits.py
@@ -1,80 +1,92 @@
 import random
 
 def generate_secret_number():
-  digits = random.choices(range(10),k=5)
-  return ''.join(map(str,digits))
+    # Generate a random 5-digit secret number
+    digits = random.choices(range(10), k=5)
+    return ''.join(map(str, digits))
 
 
 def get_user_guess():
-  while True:
-    guess = input("5 haneli bir sayı giriniz..")
-    if guess.isdigit() and len(guess) == 5:
-      return guess
-    else:
-      print("Geçersiz giriş.")
+    while True:
+        # Prompt the user to enter a 5-digit guess
+        guess = input("Enter a 5-digit number: ")
+        if guess.isdigit() and len(guess) == 5:
+            return guess
+        else:
+            print("Invalid input. Please enter a valid 5-digit number.")
 
-def compare_numbers(gsn,guess):
-  used_indices = []
-  correct_count = 0
-  feedback = ''
-  
-  
-  for i in range(5):
-    if guess[i] == gsn[i]:
-      feedback += '+'
-      used_indices.append(i)
-      correct_count += 1
-    elif guess[i] in gsn and guess[i] != gsn[i]:
-      feedback += '?'
-      used_indices.append(i)
-    else:
-      feedback += '-'
-    
-    if correct_count ==4:
-      feedback = feedback.replace("?","-")
-  return feedback
+
+def compare_numbers(secret_number, guess):
+    used_indices = []
+    correct_count = 0
+    feedback = ''
+
+    # Compare each digit of the guess with the secret number
+    for i in range(5):
+        if guess[i] == secret_number[i]:
+            feedback += '+'
+            used_indices.append(i)
+            correct_count += 1
+        elif guess[i] in secret_number and guess[i] != secret_number[i]:
+            feedback += '?'
+            used_indices.append(i)
+        else:
+            feedback += '-'
+
+        # If four digits are correct, replace '?' with '-' to avoid confusion
+        if correct_count == 4:
+            feedback = feedback.replace("?", "-")
+
+    return feedback
 
 
 def play_game():
-  while True:
-
-    print("""**************************************** 
+    while True:
+        # Display the game menu
+        print("""****************************************
   /-/- Welcome to the Mystery Digits! -\-\ 
-  **************************************** 
+  ****************************************
       (1) Play Game
       (2) About
       (3) Quit
-    
-    """ )
+    """)
 
-    choice = input("Enter your choice..")
-    if choice == "1":
-      
-        secret_number = generate_secret_number()
-        attempts = 0
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            # Start a new game
+            secret_number = generate_secret_number()
+            attempts = 0
 
-        while True:
-          guess = get_user_guess()
-          attempts += 1
+            while True:
+                # Get user's guess and provide feedback
+                guess = get_user_guess()
+                attempts += 1
 
-          if guess == secret_number:
-            print("Tebrikler...")
+                if guess == secret_number:
+                    print("Congratulations! You guessed the correct number.")
+                    break
+                feedback = compare_numbers(secret_number, guess)
+                print(f"Result: {feedback}")
+
+            print(f"Game over. Total attempts: {attempts}")
+
+        elif choice == "2":
+            # Provide information about the game
+            print("""About Mystery Digits:
+      - Mystery Digits is a number guessing game where you have to guess a 5-digit number.
+      - For each guess, you will receive feedback in the form of '+' for correct digits in the correct position,
+        '?' for correct digits in the wrong position, and '-' for incorrect digits.
+      - Try to guess the secret number in the fewest attempts possible.
+      Have fun!""")
+
+        elif choice == "3":
+            # Quit the game
+            print("Quitting the game...")
             break
-          feedback = compare_numbers(secret_number,guess)
-          print(f"Sonuç: {feedback}")
-    
-        print(f"Oyun bitti. Toplam deneme sayısı : {attempts}")
-    
-    elif choice == "2":
-      print(""" About Mystery Digits:        
-      - Mystery Digits is a number guessing game where you have to guess a 5-digit number.        
-      - For each guess, you will receive feedback in the form of '+' for correct digits in the correct position, '?' for correct digits in the wrong position, and '-' for incorrect digits.        
-      - Try to guess the secret number in the fewest attempts possible.        
-      Have fun! """)
-    elif choice == "3":
-      print("Quitting the game...")
-      break
-    else:
-      print("Invalid choice. Please try again...")  
 
+        else:
+            print("Invalid choice. Please try again...")
+
+
+# Start the game
 play_game()
